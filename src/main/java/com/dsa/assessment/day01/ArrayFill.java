bc591cdf551748d6dffb1cca27aadd64134b9c5a
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-08-06 20:20:42 +0200

    solved: solved ArrayFill

--- src/main/java/com/dsa/assessment/day01/ArrayFill.java
+++ src/main/java/com/dsa/assessment/day01/ArrayFill.java
@@ -19,6 +19,7 @@ public class ArrayFill {
      */
     public int[] fill(int size, int value) {
         // TODO: Implement this method
+        if (size < 0) throw new IllegalArgumentException("Value cannot be less than 0.");
         int[] result = new int[size];
         for (int i = 0; i<size; i++) {
             result[i] = value;
