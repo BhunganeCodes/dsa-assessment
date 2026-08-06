6888573232e761523f4a80f21c1d60f46036a9f9
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-08-06 20:10:06 +0200

    solved: solved ArrayEquals

--- src/main/java/com/dsa/assessment/day01/ArrayEquals.java
+++ src/main/java/com/dsa/assessment/day01/ArrayEquals.java
@@ -20,6 +20,8 @@ public class ArrayEquals {
     public boolean equals(int[] arr1, int[] arr2) {
         // TODO: Implement this method
         if (arr1 == null && arr2 == null) return true;
+        if (arr1 == null && arr2 != null) return false;
+        if (arr1 != null && arr2 == null) return false;
         if (arr1.length != arr2.length) return false;
         for (int i=0; i< arr1.length; i++) {
             if (arr1[i] != arr2[i]) return false;
