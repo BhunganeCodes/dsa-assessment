aab94c18c057ce16114017940b8c698bfde086a0
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-08-06 20:09:07 +0200

    solved: first attempt ArrayEquals

--- src/main/java/com/dsa/assessment/day01/ArrayEquals.java
+++ src/main/java/com/dsa/assessment/day01/ArrayEquals.java
@@ -19,6 +19,11 @@ public class ArrayEquals {
      */
     public boolean equals(int[] arr1, int[] arr2) {
         // TODO: Implement this method
-        throw new UnsupportedOperationException("TODO: Implement equals(int[] arr1, int[] arr2)");
+        if (arr1 == null && arr2 == null) return true;
+        if (arr1.length != arr2.length) return false;
+        for (int i=0; i< arr1.length; i++) {
+            if (arr1[i] != arr2[i]) return false;
+        }
+        return true;
     }
 }
