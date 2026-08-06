9fa3436f05774d9acf103334acf70c33329b5564
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-08-06 20:57:38 +0200

    solved: ArraySecondLargest first attempt

--- src/main/java/com/dsa/assessment/day01/ArraySecondLargest.java
+++ src/main/java/com/dsa/assessment/day01/ArraySecondLargest.java
@@ -1,5 +1,8 @@
 package com.dsa.assessment.day01;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Day 01 - ArraySecondLargest
  * 
@@ -13,12 +16,19 @@ public class ArraySecondLargest {
 
     /**
      * TODO: Implement findSecondLargest(int[] array)
-     * 
+     *
      * @param array the input array
      * @return int
      */
     public int findSecondLargest(int[] array) {
         // TODO: Implement this method
-        throw new UnsupportedOperationException("TODO: Implement findSecondLargest(int[] array)");
+       if (array == null || array.length <= 2) throw new IllegalArgumentException();
+        Arrays.sort(array);
+        int[] res = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            res[i] = array[i];
+        }
+        return res[res.length - 2];
     }
 }
