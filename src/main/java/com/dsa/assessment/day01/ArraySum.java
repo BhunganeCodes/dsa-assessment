4420584c57975659f9e7a23dac3045775d21e2a1
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-08-07 18:28:16 +0200

    day01: second attempt ArraySum

--- src/main/java/com/dsa/assessment/day01/ArraySum.java
+++ src/main/java/com/dsa/assessment/day01/ArraySum.java
@@ -21,7 +21,7 @@ public class ArraySum {
         // TODO: Implement this method
         int result = 0;
 
-        for (int i = 0; i < array.length - 1; i++) {
+        for (int i = 0; i < array.length; i++) {
             result += array[i];
         }
         return result;
