045bccaf0afdc5c67b19c80ebca707f73fcd2dfd
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-08-09 19:54:57 +0200

    day02: FindSingleNumber solved by adding an else block in the first for-loop to eradicate the bug.

--- src/main/java/com/dsa/assessment/day02/FindSingleNumber.java
+++ src/main/java/com/dsa/assessment/day02/FindSingleNumber.java
@@ -28,9 +28,10 @@ public class FindSingleNumber {
         for (int num : array) {
             if (map.containsKey(num)) {
                 map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+                single = num;
             }
-            map.put(num, 1);
-            single = num;
         }
 
         for (int key : map.keySet()) {
