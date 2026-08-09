b045c0249af0a3da0808f72422c627d18fc8a52a
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-08-09 18:56:04 +0200

    day02: FindCeil solved.

--- src/main/java/com/dsa/assessment/day02/FindCeil.java
+++ src/main/java/com/dsa/assessment/day02/FindCeil.java
@@ -19,6 +19,15 @@ public class FindCeil {
      */
     public int findCeil(int[] sortedArray, int target) {
         // TODO: Implement this method
-        throw new UnsupportedOperationException("TODO: Implement findCeil(int[] sortedArray, int target)");
+        if (sortedArray == null) return 0;
+        int res = 0;
+
+        for (int num : sortedArray) {
+            if (num >= target) {
+                res = num;
+                break;
+            }
+        }
+        return res;
     }
 }
